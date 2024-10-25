515b4d20b40eba0551f03058ed499e32310884f4
Abdullah73k <abdullahbkhamis@gmail.com>
2024-10-24 21:29:09 -0400

    Initial commit

---
+++ Source/UE5_Survival_Game/UE5_Survival_GameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_Survival_GameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUE5_Survival_GameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUE5_Survival_GameGameMode();
+};
+
+
+
